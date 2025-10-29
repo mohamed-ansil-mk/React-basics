4f2f0f7b95bd1a747e5e8dbad06cea06f6f0c16c
mohamed ansil <mohamedansil007@gmail.com>
2025-10-29 20:47:19 +0530

    ater conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,22 +1,19 @@
 function ListGroup() {
-    const items = [
-
-        "NewYork",
-        "SanFrancisco",
-        "Tokyo",
-        "London",
-        "Paris"
-        ];
-
+    let items = ["NewYork","SanFrancisco","Tokyo","London","Paris"];
+    items = [];
+    if (items.length === 0)
+    return <p>item not found</p>
         
-  return (
-    <>
-    <h1>List</h1>
-    <ul className="list-group">
-      {items.map(item => (
-      <li key={item}>{item}</li>))  }
-    </ul>
-    </>
+    return (
+     <>
+     <h1>List</h1>
+     {items.length  === 0 ? <p> no item found</p> : null }
+     {items.length  === 0 &&  <p> no iten found</p>  }
+        <ul className="list-group">
+            {items.map(item => (
+            <li key={item}>{item}</li>))  }
+        </ul>
+     </>
   );
 }
 
