297df73905ebb9638ef7358c9f88ff9969733b2c
mohamed ansil <mohamedansil007@gmail.com>
2025-10-29 17:44:26 +0530

    first commit

---
+++ src/message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing
+
+function MessageBox(){
+    const name = "Ansil"
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+
+export default MessageBox;
