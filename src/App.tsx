43707104f56c192a43752a34226405c777fd51ea
mohamed ansil <mohamedansil007@gmail.com>
2025-10-30 20:23:30 +0530

    add children component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import ListGroup  from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App(){
-  let items = ["NewYork","SanFrancisco","Tokyo","London","Paris"];
-  const handleSelectItem = (item:string) => {
-    console.log(item);
-  }
-  return (<div>
-          <ListGroup items={items}  heading="cities" onSelectItem={handleSelectItem}/>
+ 
+  
+  return (
+        <div >
+          <Alert>
+            Hello <span>World</span>
+          </Alert>
         </div>);
 
 }
