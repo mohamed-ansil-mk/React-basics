8e19a7e563f3ef57d4074034aa356c8ee8205ee5
mohamed ansil <mohamedansil007@gmail.com>
2025-10-30 22:20:20 +0530

    add the button with different colors according to bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App(){
  
   
   return (
         <div >
-          <Alert>
-            Hello <span>World</span>
-          </Alert>
+          <Button  onClick={() => console.log("clicked")}> My Button </Button>
         </div>);
 
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface props{
+    children : string;
+    color?: string;
+    onClick:() => void;
+    
+}
+
+const Button = ({children , onClick , color="primary"}:props) => {
+  return (
+    <button className={'btn  btn-'+ color }onClick={onClick}>{children}</button>
+  )
+}
+
+export default Button
